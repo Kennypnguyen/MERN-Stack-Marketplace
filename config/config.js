767fc3556bb0c2856a5f8b1dc1b1a6465fd52eff
Kennypnguyen <70982762+Kennypnguyen@users.noreply.github.com>
2025-02-11 23:34:35 -0800

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "example_secret_key",
+  mongoUri: process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject',
+  stripe_connect_test_client_id: 'example_stripe_connect_test_client',
+  stripe_test_secret_key: 'example_stripe_test_secret_key',
+  stripe_test_api_key: 'example_stripe_test_api_key'
+}
+
+export default config
